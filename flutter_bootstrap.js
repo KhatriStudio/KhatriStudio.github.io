5499cf4a1e33a22dc4ef0dfafeee8abc84af64c0
KhatriStudio <104985194+KhatriStudio@users.noreply.github.com>
2025-02-17 20:25:13 +0530

    Updated

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2143687270"
+    serviceWorkerVersion: "3054452198"
   }
 });
